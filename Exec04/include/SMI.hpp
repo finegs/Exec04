b37618c88d0e83727fcfa4b117a42e49297ba87a
KANGSOONGOO <finegs@gmail.com>
2018-12-22 10:26:30 +0900

    ** boost::interprocess::shared_memory_object done

---
+++ Exec04/include/SMI.hpp
@@ -0,0 +1,23 @@
+/*
+ * SMI.hpp
+ *
+ *  Created on: 2018. 12. 22.
+ *      Author: finegs
+ */
+
+#ifndef INCLUDE_SMI_HPP_
+#define INCLUDE_SMI_HPP_
+
+class MyClass
+{
+	//...
+};
+
+int f01();
+int f02();
+int f03();
+
+
+
+
+#endif /* INCLUDE_SMI_HPP_ */
---
+++ Exec04/src/ipc_smi/AppSMI.cpp
@@ -0,0 +1,17 @@
+/*
+ * AppSMI.cpp
+ *
+ *  Created on: 2018. 12. 22.
+ *      Author: finegs
+ */
+
+#include <SMI.hpp>
+
+
+int main(int argc, char* argv[])
+{
+//	return f01();
+//	return f02();
+	return f03();
+}
+
